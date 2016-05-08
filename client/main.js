e9fc7874d37003dbe9a6af61ce8234116e4b236c
Humberto Zayas <soundsinhd@gmail.com>
2016-05-07 22:51:45 -0400

    Initial Commit
    First commit with Brandon’s setup.

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import '/imports/layouts/layout1.html';
+import './main.html';
+import '/imports/startup/client/routes';
+
+
+
+
---
+++ imports/startup/client/routes.js
@@ -0,0 +1,17 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+
+FlowRouter.route('/', {
+    name: 'home',
+    action: function () {
+        BlazeLayout.render('layout1', { top: "header", main: "landing" });
+    }
+});
+
+FlowRouter.route('/test/', {
+    name: 'blogpost',
+    action: function () {
+        BlazeLayout.render('layout1', { top: "header", main: "postList" });
+    }
+});
